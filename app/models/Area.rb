7a3725f3fe202fd9ca372bf4f033e1ba2b42c758
cassandrawhitty <cassandraallexwhitty@gmail.com>
2020-11-12 10:08:51 -0700

    Working on methods for accessing areas using sport names

--- app/models/Area.rb
+++ app/models/Area.rb
@@ -1,3 +1,11 @@
 class Area < ActiveRecord::Base
     belongs_to :sport
+
+    def self.all_areas_by_sport
+        if all.sport_id = Sport
+          all.pluck(:area_name)
+        else 
+          nil
+        end
+    end
 end
--- runner.rb
+++ runner.rb
@@ -5,6 +5,7 @@ app = Cli.new
 app.welcome
 app.start
 app.pick_a_sport
+app.pick_an_area
 
 binding.pry
 
